d2b1fa772d6c0c988686867fe33b3bc29c3b97de
qyvlik <qyvlik@qq.com>
2022-06-15 16:26:57 +0800

    feat: save code

---
+++ app.js
@@ -0,0 +1,11 @@
+import JsonRpc from './lib/jsonrpc/JsonRpc.js'
+
+import WebSocket from "ws";
+import {WebSocketServer} from 'ws';
+
+(async ()=>{
+
+    const server = JsonRpc.createServer(8080);
+
+
+})();
