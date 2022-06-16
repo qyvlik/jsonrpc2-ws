f0e35223953260de3beab83bf66b893eaf8ac77c
qyvlik <qyvlik@qq.com>
2022-06-16 21:51:59 +0800

    feat: save code

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 import JsonRpcWS from "./lib/jsonrpc-ws/JsonRpcWS.js";
 import JsonRpc from "./lib/jsonrpc-ws/JsonRpc.js";
-import JsonRpcOnConnectionInterceptor from "./lib/jsonrpc-ws/JsonRpcOnConnectionInterceptor.js";
-import JsonRpcOnMessageInterceptor from "./lib/jsonrpc-ws/JsonRpcOnMessageInterceptor.js";
-import JsonRpcOnRequestInterceptor from "./lib/jsonrpc-ws/JsonRpcOnRequestInterceptor.js";
-import {jsonrpc} from "./lib/jsonrpc-ws/JsonRpcConst.js";
+import JsonRpcOnConnectionInterceptor from "./lib/jsonrpc-ws/interceptors/JsonRpcOnConnectionInterceptor.js";
+import JsonRpcOnMessageInterceptor from "./lib/jsonrpc-ws/interceptors/JsonRpcOnMessageInterceptor.js";
+import JsonRpcOnRequestInterceptor from "./lib/jsonrpc-ws/interceptors/JsonRpcOnRequestInterceptor.js";
+import {JSON_RPC_ERROR_METHOD_INVALID_PARAMS, jsonrpc} from "./lib/jsonrpc-ws/JsonRpcConst.js";
 
 const server = new JsonRpc({role: 'server'});
+
+async function sleep(ms) {
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, ms);
+    });
+}
 
 server.onConnectionInterceptor = new JsonRpcOnConnectionInterceptor(async (websocket, request) => {
     websocket.ctx = {
         authorized: false,
         times: 0,
-        time: Date.now()
+        time: Date.now(),
+        timer: null
     }
     return true;
+}, async (websocket) => {
+    clearInterval(websocket.ctx.timer);
 });
 
 server.onMessageInterceptor = new JsonRpcOnMessageInterceptor(async (websocket, data, isBinary) => {
@@ -28,17 +37,52 @@ server.onMessageInterceptor = new JsonRpcOnMessageInterceptor(async (websocket, 
 
 server.onRequestInterceptor = new JsonRpcOnRequestInterceptor(async ({id, method, params}, websocket) => {
     return true;
-}, async ({id, method, params}, websocket, response) => {
+}, async ({id, method, params}, websocket, result) => {
 })
 
 server.addMethod('ping', (params, websocket) => {
     return Date.now();
 }, 1);
 
+server.addMethod('sleep', async (params, websocket) => {
+    const [ms] = params;
+    await sleep(ms);
+    return true;
+}, 1);
+
+server.addMethod('interval_echo', async (params, websocket) => {
+    const {enable, timeout} = params;
+    if (typeof enable === 'undefined' || enable === null) {
+        throw {jsonrpc, error: JSON_RPC_ERROR_METHOD_INVALID_PARAMS, message: "Invalid Params: enable"};
+    }
+
+    if (enable && (typeof timeout === 'undefined' || timeout === null || timeout < 1)) {
+        throw {jsonrpc, error: JSON_RPC_ERROR_METHOD_INVALID_PARAMS, message: "Invalid Params: timeout"};
+    }
+
+    if (enable && websocket.ctx.timer == null) {
+        websocket.ctx.timer = setInterval(async () => {
+            try {
+                await server.notification('heart', Date.now(), websocket);
+            } catch (error) {
+                console.error(`notification error`, error)
+            }
+        }, timeout);
+        return true;
+    }
+
+    if (!enable) {
+        clearInterval(websocket.ctx.timer);
+        websocket.ctx.timer = null;
+    }
+
+    return true;
+}, 1);
+
 server.addMethod("auth", async (params, websocket) => {
     const {token} = params;
     if (token) {
-        websocket.state['authorized'] = true;
+        websocket.ctx.authorized = true;
         return true;
     }
     return false;
--- tests/test.js
+++ tests/test.js
@@ -1,16 +1,35 @@
 import JsonRpcWS from '../lib/jsonrpc-ws/JsonRpcWS.js'
 import JsonRpc from "../lib/jsonrpc-ws/JsonRpc.js";
 
-(async () => {
+process.on('unhandledRejection', (reason, p) => {
+    console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
+    // application specific logging, throwing an error, or other logic here
+});
 
-    const client = new JsonRpc({role: 'client'});
+const client = new JsonRpc({role: 'client'});
+client.addMethod("heart", async (params) => {
+    console.info(`heart:`, params);
+});
 
-    await JsonRpcWS.connectServer(client, "ws://localhost:8080");
+await JsonRpcWS.connectServer(client, "ws://localhost:8080");
 
-    const result = await client.request('ping', []);
-    // let d = 100;
-    // while (d-- > 0) {
-    //     console.info(`result=${result}`);
-    // }
+try {
+    let d = 100;
+    while (d-- > 0) {
+        const result = await client.request('ping', []);
+        console.info(`result=${result}`);
+    }
 
-})();
+    await client.request('sleep', [1000]);
+    console.info(`call sleep`);
+
+} catch (error) {
+    console.error(error)
+}
+
+await client.request('interval_echo', {enable: true, timeout: 500});
+
+await client.request('sleep', [5000]);
+
+await client.request('interval_echo', {enable: false, timeout: 500});
+
