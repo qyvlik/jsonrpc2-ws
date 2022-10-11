727cdc1e9549e7e1a3e4440900b768d7daa1dffd
qyvlik <qyvlik@qq.com>
2022-10-11 15:51:31 +0800

    feat: save test case

--- tests/server.test.js
+++ tests/server.test.js
@@ -2,6 +2,7 @@ import JsonrpcServer from "../src/lib/jsonrpc2-ws/jsonrpc-server.js";
 import JsonrpcClient from "../src/lib/jsonrpc2-ws/jsonrpc-client.js";
 import WebSocket from "ws";
 import {
+    JSON_RPC_ERROR,
     JSON_RPC_ERROR_METHOD_INVALID_PARAMS, JSON_RPC_ERROR_METHOD_NOT_FOUND,
 } from "../src/lib/jsonrpc2-ws/constant.js";
 
@@ -43,22 +44,74 @@ test('test server method', () => {
 
     const sleep = async (ms) => new Promise(resolve => setTimeout(resolve, ms));
     const echo = (params) => params;
-    const error = () => {
-        throw new Error(`error!`)
+    const error = (params) => {
+        throw new Error(`${params[0]}`)
     };
+    const time = () => Date.now();
 
     server.addMethod('echo', echo);
     server.addMethod('sleep', sleep);
     server.addMethod('error', error);
+    server.addMethod('time', time);
+
+    try {
+        server.addMethod('null', null);
+    } catch (error) {
+        expect(error.message).toBe('method not function');
+    }
+    try {
+        server.addMethod('undefined', undefined);
+    } catch (error) {
+        expect(error.message).toBe('method not function');
+    }
+    try {
+        server.addMethod('1', 1);
+    } catch (error) {
+        expect(error.message).toBe('method not function');
+    }
+    try {
+        server.addMethod('string', `string`);
+    } catch (error) {
+        expect(error.message).toBe('method not function');
+    }
 
     expect(server.methods.has('echo')).toBe(true);
     expect(server.methods.has('sleep')).toBe(true);
     expect(server.methods.has('error')).toBe(true);
+    expect(server.methods.has('time')).toBe(true);
+    expect(server.methods.size).toBe(4);
+    expect(server.methods.has('method_not_found')).toBe(false);
 });
 
 const clients = new Set();
 
-test('test client startup', async () => {
+test('test client call server method', async () => {
+    const client = await startupClient(`ws://localhost:${port}`);
+    expect(client).not.toBe(null);
+    expect(client.ws.readyState).toBe(WebSocket.OPEN);
+    clients.add(client);
+
+    {
+        const sleepMs = 1000;
+        const gap = 10;
+        const start = Date.now();
+        const result = await client.request('sleep', [sleepMs]);
+        const end = Date.now();
+        const cost = (end - start);
+        expect(result).toBeNull();
+        expect(Math.abs(cost - sleepMs)).toBeLessThan(gap);
+    }
+
+    {
+        const gap = 10;
+        const start = Date.now();
+        const result = await client.request('time', []);
+        const realGap = (result - start);
+        expect(realGap).toBeLessThan(gap);
+    }
+});
+
+test('test client invalid params', async () => {
     const client = await startupClient(`ws://localhost:${port}`);
     expect(client).not.toBe(null);
     expect(client.ws.readyState).toBe(WebSocket.OPEN);
@@ -66,6 +119,7 @@ test('test client startup', async () => {
 
     try {
         await client.request('lost_params');
+        expect(false).toBe(true);
     } catch (error) {
         const {code, message} = error;
         expect(code).toBe(JSON_RPC_ERROR_METHOD_INVALID_PARAMS);
@@ -73,7 +127,43 @@ test('test client startup', async () => {
     }
 
     try {
+        await client.request('params_is_string', '');
+        expect(false).toBe(true);
+    } catch (error) {
+        const {code, message} = error;
+        expect(code).toBe(JSON_RPC_ERROR_METHOD_INVALID_PARAMS);
+        expect(message).toBe('Invalid params');
+    }
+
+    try {
+        await client.request('params_is_number', 111);
+        expect(false).toBe(true);
+    } catch (error) {
+        const {code, message} = error;
+        expect(code).toBe(JSON_RPC_ERROR_METHOD_INVALID_PARAMS);
+        expect(message).toBe('Invalid params');
+    }
+
+    try {
+        await client.request('params_is_number', 111.1);
+        expect(false).toBe(true);
+    } catch (error) {
+        const {code, message} = error;
+        expect(code).toBe(JSON_RPC_ERROR_METHOD_INVALID_PARAMS);
+        expect(message).toBe('Invalid params');
+    }
+});
+
+
+test('test client call not exist method', async () => {
+    const client = await startupClient(`ws://localhost:${port}`);
+    expect(client).not.toBe(null);
+    expect(client.ws.readyState).toBe(WebSocket.OPEN);
+    clients.add(client);
+
+    try {
         await client.request('method_not_found', []);
+        expect(false).toBe(true);
     } catch (error) {
         const {code, message} = error;
         expect(code).toBe(JSON_RPC_ERROR_METHOD_NOT_FOUND);
@@ -81,6 +171,23 @@ test('test client startup', async () => {
     }
 });
 
+test('test client call error', async () => {
+    const client = await startupClient(`ws://localhost:${port}`);
+    expect(client).not.toBe(null);
+    expect(client.ws.readyState).toBe(WebSocket.OPEN);
+    clients.add(client);
+
+    const error_string = Date.now() + '';
+    try {
+        await client.request('error', [error_string]);
+        expect(false).toBe(true);
+    } catch (error) {
+        const {code, message, data} = error;
+        expect(code).toBe(JSON_RPC_ERROR);
+        expect(message).toBe('Server error');
+        expect(data.message).toBe(error_string);
+    }
+});
 
 afterAll(async () => {
     if (server != null) {
